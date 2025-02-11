83bf6aab9d80f272e54f765e00bedebb460f8744
Aaran Bradford <2849170b@student.gla.ac.uk>
2025-02-11 22:57:15 +0000

    Chapter 9 tests OK

--- rango/views.py
+++ rango/views.py
@@ -41,6 +41,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -52,6 +53,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -121,7 +123,8 @@ def user_login(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    context_dict = {'loggedmessage': "Since you're logged in, you can see this text!"}
+    return render(request, 'rango/restricted.html', context=context_dict)
 
 @login_required
 def user_logout(request):
